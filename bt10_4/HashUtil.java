99793caceecec0a2a5e1ed312094364be053df8b
DucLong9209 <longtd.24ite@vku.udn.vn>
2025-04-10 09:49:18 +0700

    baitapjava

---
+++ bt10_4/HashUtil.java
@@ -0,0 +1,23 @@
+package bt10_4;
+
+
+import java.security.MessageDigest;
+
+class HashUtil {
+    public static String hashPassword(String password) {
+        try {
+            MessageDigest md = MessageDigest.getInstance("SHA-256");
+            byte[] hashBytes = md.digest(password.getBytes());
+
+            StringBuilder sb = new StringBuilder();
+            for (byte b : hashBytes) {
+                sb.append(String.format("%02x", b));
+            }
+
+            return sb.toString();
+        } catch (Exception e) {
+            e.printStackTrace();
+            return null;
+        }
+    }
+}
